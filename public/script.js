4a66d43d9116debc52268533c42bcb284fc93385
Thompson B. Crozier <cybertrebuchet@gmail.com>
2016-02-02 17:05:42 -0500

    add rudimentary Backbone router a la https://www.youtube.com/watch?v=FZSjvWtUxYk (15 minutes in)

--- public/script.js
+++ public/script.js
@@ -1,3 +1,18 @@
 'use strict';
 
-console.log('Here I am in ur internetz!');
+$(function(){
+  console.log('Here I am in ur internetz!');
+
+  var Router = Backbone.Router.extend({
+    routes: {
+      '': 'home'
+    }
+  });
+
+  var router = new Router();
+  router.on('route:home', function(){
+    console.log('Backbone home route hit!');
+  });
+
+  Backbone.history.start();
+});
