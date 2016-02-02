ac4798f02cc22f8da36ff321e700e2414afb3c9c
Thompson B. Crozier <cybertrebuchet@gmail.com>
2016-02-02 17:42:44 -0500

    scaffolding Blinks view

--- public/script.js
+++ public/script.js
@@ -3,6 +3,30 @@
 $(function(){
   console.log('Here I am in ur internetz!');
 
+  var Blink = Backbone.Model.extend({
+    defaults: {
+      content: null,
+      author: null,
+      date: null,
+      nods: 0,
+      parent: null
+    }
+  });
+
+  var Blinks = Backbone.Collection.extend({
+    url: '/blinks',
+    model: Blink
+  });
+
+  var BlinksList = Backbone.View.extend({
+    el: '.blinks',
+    render: function(){
+      this.$el.html('All Blinks will show here...');
+    }
+  });
+
+  var blinksList = new BlinksList();
+
   var Router = Backbone.Router.extend({
     routes: {
       '': 'home'
@@ -10,8 +34,10 @@ $(function(){
   });
 
   var router = new Router();
+
   router.on('route:home', function(){
-    console.log('Backbone home route hit!');
+    console.log('Backbone home route hit! Rendering Blinks...');
+    blinksList.render();
   });
 
   Backbone.history.start();
