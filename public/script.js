a1dabcb373af690a7212fbe4b7d713c09b19f466
Thompson B. Crozier <cybertrebuchet@gmail.com>
2016-02-04 10:10:02 -0500

    posts seem to work, but upon refresh they disappear

--- public/script.js
+++ public/script.js
@@ -3,20 +3,20 @@
 $(function(){
   console.log('Here I am in ur internetz!');
 
-  // This function turns form data into JSON for the saveU
+  // This function turns form data into JSON for BlinksList.saveBlink()
   // Seen at: https://github.com/thomasdavis/backbonetutorials/tree/gh-pages/videos/beginner#jquery-serializeobject
   $.fn.serializeObject = function() {
     var o = {};
     var a = this.serializeArray();
     $.each(a, function() {
-        if (o[this.name] !== undefined) {
-            if (!o[this.name].push) {
-                o[this.name] = [o[this.name]];
-            }
-            o[this.name].push(this.value || '');
-        } else {
-            o[this.name] = this.value || '';
+      if (o[this.name] !== undefined) {
+        if (!o[this.name].push) {
+          o[this.name] = [o[this.name]];
         }
+        o[this.name].push(this.value || '');
+      } else {
+        o[this.name] = this.value || '';
+      }
     });
     return o;
   };
@@ -34,41 +34,71 @@ $(function(){
 
   var Blinks = Backbone.Collection.extend({
     url: '/blinks',
-    model: Blink
+    model: Blink,
+    parse: function(data){
+      return data.blinks;
+    }
+  });
+
+  var BlinkView = Backbone.View.extend({
+    tagName: 'li',
+    className: 'blink',
+    template: _.template($('#blink-template').html()),
+
+    initialize: function() {
+      this.listenTo(this.model, 'destroy', this.remove)
+    },
+
+    render: function() {
+      var html = this.template(this.model.toJSON());
+      this.$el.html(html);
+      return this;
+    },
+
+    events: {
+      'click .remove': 'onRemove'
+    },
+
+    onRemove: function() {
+      this.model.destroy();
+    }
   });
 
   var BlinksList = Backbone.View.extend({
     el: '.blinks',
-    render: function(){
-      var that = this;
-      var blinks = new Blinks();
-      blinks.fetch({
-        success: function(blinks){
-          console.log('Yay we fetched Blinks! And here they are:');
-          console.log(blinks);
-          var template = _.template($('#blink-list-template').html(), {
-            blinks: blinks.models
-          });
-          that.$el.html(template);
-        }
-      })
+
+    initialize: function() {
+      this.listenTo(this.collection, 'sync change', this.render);
+      this.collection.fetch();
+      this.render();
     },
+    render: function() {
+      var $list = this.$('ul.blinks-list').empty();
+
+      this.collection.each(function(model) {
+        var blink = new BlinkView({ model: model });
+        $list.append(blink.render().$el);
+      }, this);
+
+      return this;
+    },
+
     events: {
       'submit #new-blink-form': 'saveBlink'
     },
+
     saveBlink: function(e){
       var newBlink = $(e.currentTarget).serializeObject();
-      var blink = new Blink();
-      blink.save(newBlink, {
-        success: function(blink){
-          console.log(blink);
-        }
-      });
+      console.log(newBlink);
+      blinks.create(newBlink);
       return false; // to keep page from refreshing after event
     }
   });
 
-  var blinksList = new BlinksList();
+  var blinks = new Blinks();
+  blinks.fetch();
+
+  var blinksList = new BlinksList({ collection: blinks });
 
   var Router = Backbone.Router.extend({
     routes: {
--- routes/blinks.js
+++ routes/blinks.js
@@ -2,15 +2,24 @@
 
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
 
 var mongoose = require ('mongoose');
 var Blink = require ('../models/Blink');
 
 router.get('/', function(req, res, next) {
-  console.log('Blinks route hit!');
   Blink.find(function (err, blinks) {
     if (err) return next(err);
     res.json(blinks);
+  });
+});
+
+router.post('/', jsonParser, function(req, res, next) {
+  console.log('\n\nReceived post request to /blinks:\n\n', req.body)
+  Blink.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
   });
 });
 
